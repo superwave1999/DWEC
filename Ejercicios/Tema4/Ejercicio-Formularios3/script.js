68ad8323dc742b47be96c8eeb4b6e4732fd5f1e6
superwave1999 <superwave1999@gmail.com>
2018-11-13 09:50:07 +0100

    Add the final functions and it actually works

--- Ejercicios/Tema4/Ejercicio-Formularios3/script.js
+++ Ejercicios/Tema4/Ejercicio-Formularios3/script.js
@@ -1,18 +1,6 @@
-/*
-Al hacer clic sobre el enlace “Ver fotos” aparecerán todas las fotos (previamente están ocultas), al volver a hacer clic desaparecerán.
-Al modificar el campo "número de noches" de cualquier viaje,  se debe modificar automáticamente el precio (el precio por día está en el span con clase 'tour' correspondiente a ese viaje) .
-Al pulsar el botón de "reserve ahora", desaparecerá dicho botón y aparecerá en su lugar el mensaje: “Llame al 902 30 30 30 para reservar este viaje”.
-*/
-
 (function () {
 
     var tours = document.getElementsByClassName("tour");
-
-
-    
-
-
-
 
     for (var i=0; i<tours.length; i++) {
         var numNoches = tours[i].getElementsByTagName('input');
@@ -20,19 +8,21 @@ Al pulsar el botón de "reserve ahora", desaparecerá dicho botón y aparecerá 
             cambiarPrecio(e);
         });
 
+        var buttonRes = tours[i].getElementsByTagName("button");
+
+        if ((buttonRes[0].classList.contains('book'))) {
+            buttonRes[0].addEventListener("click", function (e) {
+                displayText(e);
+            });
+        }
+
 
     }
 
-
-
-
-
-
-
-
-
-
-
+    var photoToggle = document.getElementsByClassName("ver-fotos");
+    photoToggle[0].addEventListener("click", function (e) {
+        displayPhotos(e);
+    })
 
     function cambiarPrecio (e) {
         var clicked = e.target;
@@ -43,39 +33,42 @@ Al pulsar el botón de "reserve ahora", desaparecerá dicho botón y aparecerá 
 
         var nights = clicked.value;
 
-
         var nightsText = padre.getElementsByClassName("numero-noches");
-
-
-
         nightsText[0].textContent = nights;
-
 
         //Actualiza el precio
         spanChng[0].textContent = pricePerDay * nights;
+    }
 
+    function displayText (e) {
+        var clicked = e.target;
+        var padre = clicked.parentNode;
 
+        var button = padre.getElementsByTagName("button");
+        button[0].setAttribute("disabled","disabled");
+
+        alert('Llame al 902 30 30 30 para reservar');
 
     }
 
+    function displayPhotos (e) {
+        var target = e.target;
+        if (target.nodeName == "A") {
+            var photoList = target.nextElementSibling;
+
+            if (photoList.style.display=="") {
+                photoList.style.display = "block";
+            }
 
 
+            if (photoList.style.display == "none") {
+                photoList.style.display = "block";
+            } else if (photoList.style.display == "block") {
+                photoList.style.display = "none";
+            }
 
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 
 })();
